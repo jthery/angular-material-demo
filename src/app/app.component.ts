09021653894b8ac8ed89af28de22b2b9790369b7
Jeremy Thery <jeremy.thery@atos.net>
2019-04-11 16:28:55 +0200

    add Checkbox, Radio Button, Drop down list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'material-demo';
+  // mettre par défaut notre checkbox en cocher(true)
+  isChecked = true;
+
+  // pour choisir par defaut sur notre selecteur l'id 2
+  color = 2;
+
+  // tableau de donnée de notre drop down list
+  colors = [
+    {
+      id: 1,
+      name: 'red'
+    },
+    {
+      id: 2,
+      name: 'orange'
+    },
+    {
+      id: 3,
+      name: 'green'
+    }
+  ]
+
+  // Lors du cochement ou décochement de la checkbox, ça envoi une reponse false ou true dans la console
+  onChange($event) {
+    console.log($event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCheckboxModule, MatRadioModule, MatSelectModule } from '@angular/material';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    MatRadioModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
